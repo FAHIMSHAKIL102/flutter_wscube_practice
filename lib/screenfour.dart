68533101ff686072ae1f4f7cd949b12254e0a091
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2025-11-27 20:30:09 +0600

    add_textformfield

--- lib/screenfour.dart
+++ lib/screenfour.dart
@@ -1,13 +1,72 @@
 import 'package:flutter/material.dart';
 
 class Screenfour extends StatelessWidget {
-  const Screenfour({super.key});
+  Screenfour({super.key});
+  var emailcontroller = TextEditingController();
+  var passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ScreenFour')),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              controller: emailcontroller,
+              keyboardType: TextInputType.emailAddress,
+              enabled: true,
+              decoration: InputDecoration(
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.green),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                disabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                prefixIcon: Icon(Icons.email_outlined),
+                hintText: 'Email',
+                labelText: 'Email',
+              ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              controller: passwordController,
+              obscureText: true,
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                hintText: 'Password',
+                labelText: 'Password',
+                suffixIcon: IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.remove_red_eye),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                String yourEmail = emailcontroller.text.toString();
+                String yourPassword = passwordController.text.toString();
+                print(yourEmail);
+                print(yourPassword);
+              },
+              child: Text('Login'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
