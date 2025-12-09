1804c8def3f57fb32ed8f769402f765a2f31f6d1
FAHIMSHAKIL102 <fahimshakil102@gmail.com>
2025-12-09 14:11:02 +0600

    add_ripple__animation

--- lib/screentwelve_animation.dart
+++ lib/screentwelve_animation.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:ui';
 
 class ScreentwelveAnimation extends StatefulWidget {
   const ScreentwelveAnimation({super.key});
@@ -10,17 +11,19 @@ class ScreentwelveAnimation extends StatefulWidget {
 class _ScreentwelveAnimationState extends State<ScreentwelveAnimation>
     with SingleTickerProviderStateMixin {
   late Animation animation;
+  late Animation rippleAnimation;
   late Animation colorAnimation;
   late AnimationController animationController;
+  var radiusList = [100.0, 150.0, 200.0, 250.0, 300.0];
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     animationController = AnimationController(
       vsync: this,
-      duration: Duration(seconds: 15),
+      duration: Duration(seconds: 10),
     );
     animation = Tween(begin: 250.0, end: 10.0).animate(animationController);
+    rippleAnimation = Tween(begin: 0.0, end: 1.0).animate(animationController);
     colorAnimation = ColorTween(
       begin: Colors.blueGrey,
       end: Colors.orange,
@@ -28,7 +31,7 @@ class _ScreentwelveAnimationState extends State<ScreentwelveAnimation>
     animationController.addListener(() {
       setState(() {});
     });
-    animationController.forward();
+    //animationController.forward();
   }
 
   @override
@@ -38,12 +41,46 @@ class _ScreentwelveAnimationState extends State<ScreentwelveAnimation>
         title: Text('ScreenTwelve'),
         backgroundColor: Colors.lightGreen,
       ),
-      body: Center(
-        child: Container(
-          height: animation.value,
-          width: animation.value,
-          color: colorAnimation.value,
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Container(
+              height: animation.value,
+              width: animation.value,
+              color: colorAnimation.value,
+            ),
+          ),
+          SizedBox(height: 20),
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: radiusList
+                  .map(
+                    (radius) => Container(
+                      height: radius * rippleAnimation.value,
+                      width: radius * rippleAnimation.value,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.blue.withValues(
+                          alpha: 1.0 - rippleAnimation.value,
+                        ),
+                      ),
+                      child: Icon(Icons.add_call, color: Colors.white),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              animationController.forward();
+              setState(() {});
+            },
+            child: Text('Animate'),
+          ),
+        ],
       ),
     );
   }
